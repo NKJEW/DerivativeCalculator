7a706f47ffdf4f7a26ada34e458d67e05ff50553
soyamyboya <sawyersawman@gmail.com>
2019-04-30 10:44:48 -0700

    Add files via upload

--- src/Operation.java
+++ src/Operation.java
@@ -6,10 +6,39 @@ public enum Operation {
     sin,
     cos,
     tan,
-    arcsin,
-    arccos,
-    arctan,
+    csc,
+    sec,
+    cot,
+    asin,
+    acos,
+    atan,
+    acsc,
+    asec,
+    acot,
     log,
     ln,
-    exp
+    exp,
+    abs
 };
+
+/*
+a+b
+a-b or -a
+a*b
+a/b
+\sin(a)
+\cos(a)
+\tan(a)
+\csc(a)
+\sec(a)
+\cot(a)
+\asin(a)
+\acos(a)
+\atan(a)
+\acsc(a)
+\asec(a)
+\acot(a)
+\log_(a)(b) or \log(a)
+a^b
+|a|
+*/
